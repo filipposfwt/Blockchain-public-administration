196df5d825c89abd623ef28d8aec8eac00239385
Filippos Fotopoulos <ffotopoulos@census-labs.com>
2025-06-16 02:06:35 +0300

    chore: Update .gitignore and remove ignored files

--- frontend-diplomas/app/layouts/auth.vue
+++
@@ -1,8 +0,0 @@
-
-<script setup lang="ts"></script>
-<template>
-  <q-layout>
-    <q-page-container class="bg-grey-4"> <slot /> </q-page-container
-  ></q-layout>
-</template>
-<style scoped></style>
